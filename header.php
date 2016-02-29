f88742913627e792f38be4ba0791058b1fcf55a9
hassanamin444 <hassanamin4800489@gmail.com>
2016-02-29 12:06:49 +0500

    Added files via upload
    in nova folder just replace the header.php with this thanks

---
+++ header.php
@@ -0,0 +1,40 @@
+<header class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <a id="logo" class="pull-left" href="index.php"></a>
+                <div class="nav-collapse collapse pull-right">
+                    <ul class="nav">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="about-us.php">About Us</a></li>
+                        <li><a href="services.php">Services</a></li>
+                        <li><a href="portfolio.php">Portfolio</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="icon-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="career.php">Career</a></li>
+                                <li><a href="blog-item.php">Blog Single</a></li>
+                                <li><a href="faq.php">FAQ</a></li>
+                                <li><a href="pricing.php">Pricing</a></li>
+                                <li><a href="404.php">404</a></li>
+                                <li><a href="typography.html">Typography</a></li>
+                                <li><a href="registration.php">Registration</a></li>
+                                <li class="divider"></li>
+                                <li><a href="privacy.php">Privacy Policy</a></li>
+                                <li><a href="terms.php">Terms of Use</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="blog.php">Blog</a></li> 
+                        <li><a href="contact-us.php">Contact</a></li>
+                        <li class="login">
+                            <a data-toggle="modal" href="#loginForm"><i class="icon-lock"></i></a>
+                        </li>
+                    </ul>        
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+    </header>
