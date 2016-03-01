3c4752a94a7bab92e2c880ee6d7c9ba3c4ac7c7b
hassanamin444 <hassanamin4800489@gmail.com>
2016-03-01 10:12:46 +0500

    login2
    this is same design and code like register and log out

--- login.php
+++ login.php
@@ -1,57 +1,68 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Sign-In</title>
-</head>
-<body id="body-color">
-<div id="Sign-In">
-<fieldset style="width:30%"><legend>LOG-IN HERE</legend>
-<form method="POST" action="connectivity.php">
-User <br><input type="text" name="user" size="40"><br>
-Password <br><input type="password" name="pass" size="40"><br>
-<input id="button" type="submit" name="submit" value="Log-In">
-</form>
-</fieldset>
-</div>
-</body>
-</html> 
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', '');
-define('DB_USER','root');
-define('DB_PASSWORD','');
+<?PHP
+require_once("./include/membersite_config.php");
 
-$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
-/*
-$ID = $_POST['user'];
-$Password = $_POST['pass'];
-*/
-function SignIn()
+if(isset($_POST['submitted']))
 {
-session_start();  
-if(!empty($_POST['user']))  
-{
-	$query = mysql_query("SELECT *  FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
-	$row = mysql_fetch_array($query) or die(mysql_error());
-	if(!empty($row['userName']) AND !empty($row['pass']))
-	{
-		$_SESSION['userName'] = $row['pass'];
-		echo "You have SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
-
-	}
-	else
-	{
-		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
-	}
-}
-}
-if(isset($_POST['submit']))
-{
-	SignIn();
+   if($fgmembersite->Login())
+   {
+        $fgmembersite->RedirectToURL("login-home.php");
+   }
 }
 
 ?>
 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Login</title>
+      <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+</head>
+<body>
 
 
+<div id='fg_membersite'>
+<form id='login' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Login</legend>
+
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<div class='short_explanation'>* required fields</div>
+
+<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='username' >UserName*:</label><br/>
+    <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /><br/>
+    <span id='login_username_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='password' >Password*:</label><br/>
+    <input type='password' name='password' id='password' maxlength="50" /><br/>
+    <span id='login_password_errorloc' class='error'></span>
+</div>
+
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+<div class='short_explanation'><a href='reset-pwd-req.php'>Forgot Password?</a></div>
+</fieldset>
+</form>
+
+
+<script type='text/javascript'>
+
+    var frmvalidator  = new Validator("login");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+
+    frmvalidator.addValidation("username","req","Please provide your username");
+    
+    frmvalidator.addValidation("password","req","Please provide the password");
+
+
+</script>
+</div>
+
+</body>
+</html>
